a90b2d26eb0a53e7358222b43ffadcacbf234d38
nkit-chahal <achahal023.com>
2022-01-03 19:02:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+
+import os
+import requests
+import numpy as np
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+from flask import Flask, request, jsonify
+
+print(tf.__version__)
+
+#Load the pretrained model
+with open('fashion_model_flask.json', 'r') as f:
+    model_json = f.read()
+
+model = tf.keras.models.model_from_json(model_json)
+# load weights into new model
+model.load_weights("fashion_model_flask.h5")
+
+
+# Creating the Flask API
+#Starting the Flask application
+app = Flask(__name__)
+
+#classify_image function
+@app.route("/api/v1/<string:img_name>", methods=["POST"])
+def classify_image(img_name):
+    upload_dir = "uploads/"
+    image = plt.imread(upload_dir + img_name)
+    
+    #list of class names 
+    classes = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+    #have to reshape for predictions
+    prediction = model.predict([image.reshape(1, 28*28)])
+
+    
+    return jsonify({"object_identified":classes[np.argmax(prediction[0])]})
+
+
+app.run(port=120, debug=False)
